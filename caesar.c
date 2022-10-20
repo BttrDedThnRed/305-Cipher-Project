7d1e306521472ab68ce5f3493af359a94719ef0b
JoshuaCottrell <jcottrel@mail.umw.edu>
2022-10-20 18:33:21 -0400

    Finished decryption

--- caesar.c
+++ caesar.c
@@ -52,3 +52,52 @@ char *caesar_encrypt(char *plain, char *key) {
     return encrypted;
 
 }
+
+
+
+
+//Decryption
+char caesar_decrypt_char(char cipher, int key) {
+
+    //Standardize keys
+    key = key % 26;
+    char unencrypted;
+
+    //Checks if lowercase
+    if (cipher >= 'a' && cipher <= 'z') {
+        unencrypted = cipher - key;
+
+        //Make sure the character is a lower case letter
+        if (unencrypted < 'a') {
+            unencrypted = unencrypted + 'z' - 'a' + 1;
+        }
+    }
+    //Checks if uppercase
+    else if (cipher >= 'A' && cipher <= 'Z') {
+        unencrypted = cipher - key;
+
+        //Make sure the character is an upper case letter
+        if (unencrypted < 'A') {
+            unencrypted = unencrypted + 'Z' - 'A' + 1;
+        }
+    }
+
+    return unencrypted;
+}
+
+char *caesar_decrypt(char *cipher, char *key) {
+
+    //Allocate memory so we can actually return the unencrypted string
+    char *unencrypted = malloc(sizeof(char) * strlen(cipher));
+
+    //Convert key
+    int intKey = convert_key(key);
+
+    //Unencrypt each character in the string and add it to new string
+    for (int i = 0; i < strlen(cipher); i++) {
+        unencrypted[i] = caesar_decrypt_char(cipher[i], intKey);
+    }
+
+    return unencrypted;
+
+}
