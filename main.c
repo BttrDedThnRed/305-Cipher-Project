b6927fc367aaff936b9085fad5dde19e6371c5b4
Shane Alexander <salexand@mail.umw.edu>
2022-10-21 19:11:49 -0400

    encrypted message comes out jarbled`

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include "string.h"
 #include <stdio.h>
+#include "caesar.h"
 int main(){
 	string *s=new_plain("gusty",0);
 	printf("%s\n", s->plain);
 	printf("%d", (int) sizeof(s->plain));
 	printf("%d\n", s->len);
 	printf("%d\n",(int)sizeof(string));
+	
+	printf("Encrypted Gusty: %s\n", caesar_encrypt("Gusty", "123"));
+	//string *s2 = encrypt_string(CAESAR, "Gusty", "123");
 }
--- string.c
+++ string.c
@@ -9,11 +9,7 @@ string *new_plain(char *s, int roundup) {
 	str->plain=malloc(sizeof(s));
 	strcpy(str->plain,s);
 	str->len=strlen(s);
-	
-	//s->print = print_string;
-	//s->encrypt = encrypt_string;   
-	//s->decrypt = decrypt_string;   
-	
+	 
 	return(str);
 }
 //does same as new_plain trying to figure out how its supposed to be different
@@ -35,16 +31,46 @@ string *encrypt_string(cipher c, char *s, char *key) {
 	
 	string *str=malloc(sizeof(string));
 	str->plain=malloc(sizeof(s));
+	str->cipher=malloc(sizeof(s));
 	strcpy(str->plain,s);
 	switch (c) {
 	case CAESAR:
-	//return caesar_encrypt(char *s, char *key);
-		break;
+	
+	strcpy(str->cipher, caesar_encrypt(s, key));	
+	//str->cipher=caesar_encrypt(s, key);
+	
+	return str;
+	break;
+	
 	case AUGUSTUS:
 		break;
+	
 	case AES:
 		break;
 	}
 }
 
+string* encrypt(cipher c, char *s, char *key) {
+	
+	string *str=malloc(sizeof(string));
+	str->plain=malloc(sizeof(s));
+	strcpy(str->plain,s);
+	
+	switch (c) {
+	case CAESAR:
+		str->cipher=caesar_encrypt(s, key);
+		return str;
+		break;
+	
+	case AUGUSTUS:
+		break;
+	
+	case AES:                                                           
+		break;                                                            
+	}
+}
 
+char *decrypt_string(cipher c, string *str, char *key) {
+	
+	
+}
