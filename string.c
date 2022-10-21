348cd7e2147fed51a3c9929e4de50481415642c5
Shane Alexander <salexand@mail.umw.edu>
2022-10-20 21:28:31 -0400

    debugging new_plain

--- string.c
+++ string.c
@@ -1,5 +1,13 @@
 #include "string.h"
+#include <string.h>
+#include <stdlib.h>
 string *new_plain(char *str, int roundup) {
-	string *s = malloc(sizeof(string));
-	s->plain=*str;
+	string *s=malloc(sizeof(string));
+	//s->plain=malloc(sizeof(s->plain))
+	//s->plain=str;
+	strcpy(s->plain,str);
+	s->len=strlen(str);
+	return(s);
+}
+
 
--- string.h
+++ string.h
@@ -1,5 +1,5 @@
-fndef _MY_STRING_H_
-#define _MY_STRING_H_
+//fndef _MY_STRING_H_
+//#define _MY_STRING_H_
 typedef enum cipher { CAESAR, AUGUSTUS, AES } cipher;
 typedef enum string_type { PLAIN, CIPHER } string_type;
 typedef struct string string;
@@ -11,10 +11,10 @@ typedef struct string {
 	string* (*encrypt)(cipher c, char *s, char *key);
 	char *  (*decrypt)(cipher c, string *s, char *key);
 } string;
-string *new_plain(char *, int roundup);
-string *new_cipher(char *, int len, int roundup);
+string *new_plain(char *s, int roundup);
+string *new_cipher(char *s, int len, int roundup);
 string *encrypt_string(cipher c, char *s, char *key);
 char *decrypt_string(cipher c, string *str, char *key);
 void setiv_string(char *newiv);
 void print_C_string(char *s);
-#endif // _MY_STRING_H_
+//endif // _MY_STRING_H_
