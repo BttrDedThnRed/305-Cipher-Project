c5ffc6754e9dd0d9dfa76429616071413292680e
Shane Alexander <salexand@mail.umw.edu>
2022-10-20 23:10:56 -0400

    added comments to string.c

--- string.c
+++ string.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "caesar.h"
+//allocates memory for and creates new string object
 string *new_plain(char *s, int roundup) {
 	
 	string *str=malloc(sizeof(string));
@@ -15,7 +16,7 @@ string *new_plain(char *s, int roundup) {
 	
 	return(str);
 }
-
+//does same as new_plain trying to figure out how its supposed to be different
 string *new_cipher(char *s, int len, int roundup) {
 	
 	string *str=malloc(sizeof(string));
@@ -29,23 +30,21 @@ string *new_cipher(char *s, int len, int roundup) {
 	
 	return(str);                    
 }
-
+//makes calls to appropriate cipher interface
 string *encrypt_string(cipher c, char *s, char *key) {
 	
 	string *str=malloc(sizeof(string));
 	str->plain=malloc(sizeof(s));
 	strcpy(str->plain,s);
-	switch (cipher):
+	switch (c) {
 	case CAESAR:
-		return(caesar_encrypt(char *s, char *key));
+	//return caesar_encrypt(char *s, char *key);
 		break;
 	case AUGUSTUS:
 		break;
 	case AES:
 		break;
-
-
-	
+	}
 }
 
 
