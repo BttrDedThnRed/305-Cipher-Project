2b75eb737c53d7018081a760364e432d05e086b1
JoshuaCottrell <jcottrel@mail.umw.edu>
2022-10-23 23:25:17 -0400

    Finished most of fileIO part of strings tester

--- strings_tester.c
+++ strings_tester.c
@@ -1,15 +1,188 @@
 #include <stdio.h>
 #include <string.h>
 #include "string.h"
+#include "caesar.h" //this and augustus can/should get removed when string.h is implemented
+#include "augustus.h" 
 
 int main (int argc, char **argv) {
 
     int running = 1;
     while (running) {
-                //Checks if there is a file argument
-                if (argv[0]) {
-                    
+    if (strcmp(argv[1], "default") == 0) {
+        printf("Argument is default");
+        return 0;
+    }
+    //Checks if there is a file argument
+    else if (argv[1]) {
+
+        //File I/O definitions
+        FILE *file = fopen(argv[1], "r");
+        char * line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+
+        //Other definitions
+        int triplet = 1;
+        char *ciphers[] = {"Caesar", "Augustus", "AES"};
+
+
+        while ((read = getline(&line, &len, file)) != -1) {
+            if (line[0] == '#') {
+                continue;
+            }
+            else if (strcmp(line, "encrypt caesar\n") == 0) {
+
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
                 }
+
+                //set key
+                char *key = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    key[i] = line[i];
+                }
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set string
+                char *stringText = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    stringText[i] = line[i];
+                }
+
+                //print
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[0], "Encrypt");
+                printf("Output: %s\n\n", caesar_encrypt(stringText, key));
+
+
+            }
+            else if (strcmp(line, "encrypt augustus\n") == 0) {
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set key
+                char *key = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    key[i] = line[i];
+                }
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set string
+                char *stringText = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    stringText[i] = line[i];
+                }
+
+                //print
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[1], "Encrypt");
+                printf("Output: %s \n\n", augustus_encrypt(stringText, key));
+            }
+            else if (strcmp(line, "encrypt aes\n") == 0) {
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[2], "Encrypt");
+                printf("Don't have aes stuff so ignore\n\n");
+            }
+            else if (strcmp(line, "decrypt caesar\n") == 0) {
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set key
+                char *key = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    key[i] = line[i];
+                }
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set string
+                char *stringText = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    stringText[i] = line[i];
+                }
+
+                //print
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[0], "Decrypt");
+                printf("Output: %s \n\n", caesar_decrypt(stringText, key));
+            }
+            else if (strcmp(line, "decrypt augustus\n") == 0) {
+
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set key
+                char *key = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    key[i] = line[i];
+                }
+                
+                //get next line
+                read = getline(&line, &len, file);
+
+                //if that next line is commented ignore it and get next line that isn't commented
+                while (line[0] == '#') {
+                    read = getline(&line, &len, file);
+                }
+
+                //set string
+                char *stringText = calloc(len, sizeof(char));
+                for (int i = 0; i < (int)(strlen(line)) - 1; i++) {
+                    stringText[i] = line[i];
+                }
+
+                //print
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[0], "Decrypt");
+                printf("Output: %s \n\n", augustus_decrypt(stringText, key));
+            }
+            else if (strcmp(line, "decrypt aes\n") == 0) {
+                printf("Triplet: %d, Cipher: %s, %s \n", triplet++, ciphers[2], "Decrypt");
+                printf("Don't have aes stuff so ignore\n\n");
+            }
+
+
+        }
+
+
+    }
         	printf("Default keys:\n");
         	printf("Caesar: 1\n");
         	printf("Augustus: 12");
